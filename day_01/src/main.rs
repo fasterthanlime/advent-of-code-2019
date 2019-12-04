b900602374da7e382b3d60332afec94fd7210373
Amos Wenger <amoswenger@gmail.com>
2019-12-04 12:27:12 +0100

    amosify day_01

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,64 +1,100 @@
+use derive_more::*;
+
+// Don't be afraid to make "newtypes", especially when dealing with
+// multiple units (here, fuel and mass)
+
+// The `derive_more` crate lets you implement useful traits,
+// see https://crates.io/crates/derive_more
+//
+// It needs to be listed in the `Cargo.toml` file under `[dependencies]`.
+//
+// I use the `cargo-edit` crate to edit my `Cargo.toml`, so all I had to do
+// was `cargo add derive_more`.
+
+// Copy: copies the value instead of borrowing or moving it
+// Clone: required for copy
+// PartialEq: `==` operator, needed for `assert_eq!`
+// Debug: Needed for "{:?}" or "{:#?}" formatting in println!() etc.
+// Add: `+` operator
+// FromStr: needed for some_string.parse::<Mass>()
+#[derive(Clone, Copy, PartialEq, Debug, Add, FromStr)]
+struct Mass(pub i64);
+
+#[derive(Clone, Copy, PartialEq, Debug, Add, Sum)]
+struct Fuel(pub i64);
+
 fn main() {
-    // INPUT
-    let masses = [
-        74364, 146203, 128470, 91616, 115655, 134147, 53470, 126471, 70040, 88750, 142353, 143329,
-        86356, 118399, 97959, 148345, 117705, 87624, 63862, 71962, 106974, 66255, 119735, 78726,
-        93698, 148680, 144638, 83341, 149571, 147196, 54526, 91775, 63153, 143441, 71134, 114131,
-        120931, 109833, 106073, 64547, 126938, 52877, 89945, 59466, 79660, 147815, 55381, 100052,
-        78824, 121844, 104155, 117313, 69305, 144645, 81350, 123512, 81467, 120836, 118612, 143999,
-        90792, 71054, 138942, 56481, 71850, 85266, 77437, 86530, 147311, 133699, 126684, 58708,
-        149482, 104101, 67985, 81648, 95290, 77155, 76578, 116025, 83980, 59517, 62078, 89003,
-        126205, 122542, 116388, 144040, 102560, 77098, 127534, 56415, 85703, 85580, 86787, 72029,
-        82533, 132187, 70849, 98839,
-    ];
+    // instead of copy-pasting/reformatting the input file, you can have
+    // it as a file, (`input.txt`) in the `src/` folder, and parse it.
+    // `include_str!` is a macro, it will include that file at compile-time,
+    // so your program will still be portable.
+    let masses: Vec<_> = include_str!("input.txt")
+        .lines()
+        .map(|x| -> Mass { x.parse().expect("input lines should be valid masses") })
+        .collect();
 
-    // PART 1
-    //m is a borrow, needs dereferencing! (*m)
-    let sum: u32 = masses.iter().map(|m| compute_fuel(*m)).sum();
+    // Note:
+    // .map(|m| foobar(*m)) is the same as
+    // .map(|&m| foobar(m)) which is cleaner imho
+    //
+    // Here, we don't need that trick because the methods on Fuel
+    // take `&self`
 
-    println!("Part 1 answer: {}", sum);
+    // .fuel() returns an Option (see below), if it returns None we'll just
+    // assume it needs 0 fuel.
+    let sum: Fuel = masses.iter().map(|m| m.fuel().unwrap_or(Fuel(0))).sum();
+    println!("Part 1 answer: {:?}", sum);
 
-    // PART 2
-    let sum: u32 = masses
-        .iter()
-        .map(|m| compute_fuel_and_fuel_for_fuel(*m, 0))
-        .sum();
-
-    //Use the example to make sure we didn't mess up?
-    assert_eq!(compute_fuel_and_fuel_for_fuel(1969, 0), 966);
-
-    println!("Part 2 answer: {}", sum);
+    let sum: Fuel = masses.iter().map(|m| m.total_fuel()).sum();
+    println!("Part 2 answer: {:?}", sum);
 }
 
-fn compute_fuel(mass: u32) -> u32 {
-    /* Fuel required to launch a given module is based on its mass.
-     * Specifically, to find the fuel required for a module, take
-     * its mass, divide by three, round down, and subtract 2.
-     */
-
-    //Sweet, Rust rounds down on integer division
-    mass / 3 - 2
+impl Fuel {
+    /// Returns how much this amount of fuel weighs
+    fn mass(&self) -> Mass {
+        // x fuel weighs x mass
+        Mass(self.0)
+    }
 }
 
-fn compute_fuel_and_fuel_for_fuel(mass: u32, mut total_fuel: u32) -> u32 {
-    /* Fuel itself requires fuel just like a module - take its mass,
-     * divide by three, round down, and subtract 2. However, that fuel
-     * also requires fuel, and that fuel requires fuel, and so on.
-     */
-
-    //How much fuel does this mass require?
-    let fuel_mass = compute_fuel(mass);
-
-    //Keep track of the total amount of fuel
-    total_fuel += fuel_mass;
-
-    let mut result = total_fuel;
-    //"Any mass that would require negative fuel should instead be treated as if it requires zero fuel"
-    // (This is our recursion end condition :3)
-    if fuel_mass >= 9 {
-        //If the required fuel is heavy enough, it requires its own additional amount of fuel.
-        result = compute_fuel_and_fuel_for_fuel(fuel_mass, total_fuel);
+impl Mass {
+    /// Fuel required to launch this mass.
+    /// Returns None if "negative fuel" would be required, Some(Fuel) otherwise.
+    fn fuel(&self) -> Option<Fuel> {
+        let result = self.0 / 3 - 2;
+        if result < 0 {
+            None
+        } else {
+            Some(Fuel(result))
+        }
     }
 
-    result
+    /// Fuel required to launch this mass, including fuel
+    /// for the fuel, and so on recursively.
+    fn total_fuel(&self) -> Fuel {
+        match self.fuel() {
+            Some(fuel) => fuel + fuel.mass().total_fuel(),
+            None => Fuel(0),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_compute_fuel() {
+        assert_eq!(Mass(12).fuel(), Some(Fuel(2)));
+        assert_eq!(Mass(14).fuel(), Some(Fuel(2)));
+        assert_eq!(Mass(1969).fuel(), Some(Fuel(654)));
+        assert_eq!(Mass(100756).fuel(), Some(Fuel(33583)));
+    }
+
+    #[test]
+    fn test_compute_total_fuel() {
+        assert_eq!(Mass(14).total_fuel(), Fuel(2));
+        assert_eq!(Mass(1969).total_fuel(), Fuel(966));
+        assert_eq!(Mass(100756).total_fuel(), Fuel(50346));
+    }
 }
